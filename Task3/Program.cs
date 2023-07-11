7d29234535489efb16ee3d669596f92f0b3f577f
Ekaterina <kat-erina-07@mail.ru>
2023-07-11 23:53:15 +0400

    Function Ackerman

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine()!);
+}
+int AckFunction(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (n == 0)
+        return AckFunction(m - 1, 1);
+    return AckFunction(m - 1, AckFunction(m, n - 1));
+}
+int num1 = InputNum("Введите целое число m: ");
+int num2 = InputNum("Введите целое число n: ");
+Console.WriteLine($"A({num1},{num2}) = {AckFunction(num1, num2)}");
